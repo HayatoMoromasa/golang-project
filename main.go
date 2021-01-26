dfe005cad74d213c95f1143db4e458ed8010f5e2
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2021-01-27 01:19:37 +0900

    add 変数

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
+import "fmt"
+
 //packageの設定は一つだけ
 
-import (
-	"fmt"
-	"time"
-)
-
-//↑複数importできる
-
-/*
-hello world
-実行はgo run main.go
-*/
+//変数
 
 func main() {
-	fmt.Println("Hello world!!")
-	fmt.Println(time.Now())
+	//明示的な定義
+	//var 変数名　型　= 値
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "hello go"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int    = 200
+		s2 string = "golang"
+	)
+	fmt.Println(i2, s2)
 }
